ebd4019df339b7147fc9a156d20b1dfa01ec872f
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-20 11:20:39 -0800

    Export lzma

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+// LZMA decoders for external use
+export {
+  createLzma2Decoder,
+  createLzmaDecoder,
+  decodeLzma,
+  decodeLzma2,
+  Lzma2Decoder,
+  LzmaDecoder,
+} from './lzma/index.ts';
 export { default } from './SevenZipIterator.ts';
 export * from './types.ts';
